1f66e52ba2a7fded563a0b8958922100bc55a33d
GabrielAlgo12 <125403179+GabrielAlgo12@users.noreply.github.com>
2023-06-15 14:56:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    # ¿Qué hacer si no se encuentra la palabra?}
+    print("palabra no encontrada")
